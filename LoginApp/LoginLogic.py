0f1250a16de02659104c68d33c9d486a7334500f
Detreox <detreox@gmail.com>
2019-09-24 19:05:09 -0400

    Added Login File and CSV

--- LoginApp/LoginLogic.py
+++ LoginApp/LoginLogic.py
@@ -1,1 +1,10 @@
-print("Logic")
+import csv
+
+def open_csv():
+    with open ('/pygame/LoginApp/users.csv', 'r', encoding= 'utf-8-sig') as file:
+        reader = csv.reader(file)
+
+        for row in reader:
+            print(row)
+
+open_csv()
