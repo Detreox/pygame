6263d9be319a15abde48a5aa5058a573907e158f
Ale Alex <alejandroravelo42@gmail.com>
2019-09-24 19:09:18 -0400

    aaf

--- LoginApp/LoginInterface.py
+++ LoginApp/LoginInterface.py
@@ -1,1 +1,17 @@
-print("Interface")
+import tkinter as tk
+
+HEIGHT = 700
+WIDTH = 800
+
+root = tk.Tk()
+
+canvas = tk.Canvas(root, height=HEIGHT, width=WIDTH)
+canvas.pack()
+
+button = tk.Button(root, text="Log In", )
+button.pack()
+
+label = tk.LabelFrame(root, text="Username")
+label.place(relx=0.8, rely=0, relwidth=0.25, relheight=0.25)
+
+root.mainloop()
