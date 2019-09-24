33506e68ea23e6d43be3d36afb713716e7bdc915
Ale Alex <alejandroravelo42@gmail.com>
2019-09-24 19:36:34 -0400

    asd

--- LoginApp/LoginInterface.py
+++ LoginApp/LoginInterface.py
@@ -1,7 +1,7 @@
 import tkinter as tk
 
-HEIGHT = 700
-WIDTH = 800
+HEIGHT = 500
+WIDTH = 600
 
 root = tk.Tk()
 
@@ -11,7 +11,22 @@ canvas.pack()
 button = tk.Button(root, text="Log In", )
 button.pack()
 
-label = tk.LabelFrame(root, text="Username")
-label.place(relx=0.8, rely=0, relwidth=0.25, relheight=0.25)
 
+label = tk.LabelFrame(root, text="Username")
+label.place(relx=0.05, rely=0.05, relwidth=0.15, relheight=0.05)
+
+
+label = tk.LabelFrame(root, text="Passworld")
+label.place(relx=0.05, rely=0.10, relwidth=0.15, relheight=0.05)
+
+def Uname():
+ entry = tk.Entry(root)
+ entry.place(relx=0.20, rely=0.05, relwidth=0.15, relheight=0.05)
+
+def Upass():
+ entry = tk.Entry(root)
+ entry.place(relx=0.20, rely=0.10, relwidth=0.15, relheight=0.05)
+
+Uname()
+Upass()
 root.mainloop()
