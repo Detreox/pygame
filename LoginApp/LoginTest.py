490389cff8ef273a49ed9b687245ed5888f124a9
Detreox <detreox@gmail.com>
2019-09-24 22:07:17 -0400

    Removed CSV added database connection

--- LoginApp/LoginTest.py
+++ LoginApp/LoginTest.py
@@ -1,4 +1,15 @@
+import mysql.connector
+
+mydb = mysql.connector.connect(
+    host = "localhost",
+    user = "root",
+    passwd = "Durks.321"
+)
+
+print(mydb)
+
 print("Info on database: ")
+
 
 from LoginLogic import user_open
 
