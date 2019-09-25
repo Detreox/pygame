aca6e014300d7b4f03a0a23fa222ac0ea7cb77d4
Detreox <detreox@gmail.com>
2019-09-25 02:26:31 -0400

    Everything is working now. I just need to add a loop.

--- LoginApp/LoginInterface.py
+++ LoginApp/LoginInterface.py
@@ -1,32 +1,27 @@
-import tkinter as tk
+from tkinter import *
+from LoginLogic import data_connect
 
-HEIGHT = 500
-WIDTH = 600
+#BUTTON ACTION
+def click():
+        username = userentry.get()
+        password = passentry.get()
+        data_connect(username,password)        
 
-root = tk.Tk()
+#WINDOW
+window = Tk()
+window.title("Login")
 
-canvas = tk.Canvas(root, height=HEIGHT, width=WIDTH)
-canvas.pack()
+#LABELS
+Label (window, text="Username", fg = "black").grid(row = 0, column = 0, sticky = W)
+Label (window, text="Password", fg = "black").grid(row = 1, column = 0, sticky = W)
 
-button = tk.Button(root, text="Log In", )
-button.pack()
+#USERNAME AND PASSWORD ENTRY
+userentry = Entry(window, width = 20, bg = "white")
+userentry.grid(row = 0, column = 1, sticky = W)
+passentry = Entry(window, width = 20, bg = "white")
+passentry.grid(row = 1, column = 1, sticky = W)
 
+#LOGIN BUTTON
+Button(window, text = "LOGIN", width = 6, command = click).grid(row = 5, column = 1, sticky = W)
 
-label = tk.LabelFrame(root, text="Username")
-label.place(relx=0.05, rely=0.05, relwidth=0.15, relheight=0.05)
-
-
-label = tk.LabelFrame(root, text="Passworld")
-label.place(relx=0.05, rely=0.10, relwidth=0.15, relheight=0.05)
-
-def Uname():
- entry = tk.Entry(root)
- entry.place(relx=0.20, rely=0.05, relwidth=0.15, relheight=0.05)
-
-def Upass():
- entry = tk.Entry(root)
- entry.place(relx=0.20, rely=0.10, relwidth=0.15, relheight=0.05)
-
-Uname()
-Upass()
-root.mainloop()
+window.mainloop() 
--- LoginApp/LoginLogic.py
+++ LoginApp/LoginLogic.py
@@ -1,28 +1,20 @@
-def data_connect():
+def data_connect(username,password):
 
     import mysql.connector
 
     mydb = mysql.connector.connect(host = "localhost", user = "root", passwd = "Durks.321", database = "login")
 
-    while True:
-        username = input("Please enter your username: ")
-        password = input("Please etner your password: ")
-
-        cursor = mydb.cursor()
-
-        find_user = ("SELECT * FROM user_login WHERE user_name = %s AND user_pass = %s")
-        cursor.execute(find_user, [(username),(password)])
-        result = cursor.fetchall()
-        if result:
-            for i in result:
-                print("Welcome " + i[1])
+    cursor = mydb.cursor()
+ 
+    find_user = ("SELECT * FROM user_login WHERE user_name = %s AND user_pass = %s")
+    cursor.execute(find_user, [(username),(password)])
+    result = cursor.fetchall()
+    if result:
+        for i in result:
+            print("Welcome " + i[1])
             break
-        else:
-            print("Username and password combination not recognized.")
-
-
-    #cursor.execute("select user_name from user_login")
-
+    else:
+        print("Username and password combination not recognized.")
 
      
 
