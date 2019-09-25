d880f5359925e6f828032df1b2a9378a2bdd2353
Detreox <detreox@gmail.com>
2019-09-25 15:17:00 -0400

    User creation added.

--- LoginApp/LoginLogic.py
+++ LoginApp/LoginLogic.py
@@ -5,8 +5,11 @@ def data_connect(username,password):
     mydb = mysql.connector.connect(host = "localhost", user = "root", passwd = "Durks.321", database = "login")
 
     cursor = mydb.cursor()
+    cursor = mydb.cursor()
  
     find_user = ("SELECT * FROM user_login WHERE user_name = %s AND user_pass = %s")
+    create_user = ("INSERT INTO user_login (user_name, user_pass) VALUES (%s, %s)")
+
     cursor.execute(find_user, [(username),(password)])
     result = cursor.fetchall()
     if result:
@@ -15,8 +18,22 @@ def data_connect(username,password):
             break
     else:
         print("Username and password combination not recognized.")
+        answer = input("Would you like to create an account? y/n: \n")
+        if answer == "y":
+            new_user = input("Enter new Username: ")
+            new_pass = input("Enter new Password: ")
+            cursor.execute(create_user, [(new_user),(new_pass)])
+            mydb.commit()
+            cursor.execute(find_user, [(new_user),(new_pass)])
+            resultnew = cursor.fetchall()
+            if resultnew:
+                for j in resultnew:
+                    print("Welcome " + j[1])
+                    break
+        else:
+            print("Goodbye!")
 
-     
+
 
     
     
