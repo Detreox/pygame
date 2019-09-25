f5955130522da49e85fd21f95d421fe6a8fb404c
Detreox <detreox@gmail.com>
2019-09-25 00:45:44 -0400

    Database is finally fully functional

--- LoginApp/LoginLogic.py
+++ LoginApp/LoginLogic.py
@@ -4,16 +4,26 @@ def data_connect():
 
     mydb = mysql.connector.connect(host = "localhost", user = "root", passwd = "Durks.321", database = "login")
 
-    cursor = mydb.cursor()
-   
+    while True:
+        username = input("Please enter your username: ")
+        password = input("Please etner your password: ")
 
-    cursor.execute("select user_name from user_login")
-    result = cursor.fetchone()
-    
+        cursor = mydb.cursor()
 
-    for i in result:
-        print(i)
-        
+        find_user = ("SELECT * FROM user_login WHERE user_name = %s AND user_pass = %s")
+        cursor.execute(find_user, [(username),(password)])
+        result = cursor.fetchall()
+        if result:
+            for i in result:
+                print("Welcome " + i[1])
+            break
+        else:
+            print("Username and password combination not recognized.")
+
+
+    #cursor.execute("select user_name from user_login")
+
+
      
 
     
