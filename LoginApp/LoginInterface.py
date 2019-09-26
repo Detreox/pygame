b18a3054d1c04c2d089afa593485b9698b67cdee
Detreox <detreox@gmail.com>
2019-09-26 18:08:47 -0400

    Many changes made.

--- LoginApp/LoginInterface.py
+++ LoginApp/LoginInterface.py
@@ -1,26 +1,30 @@
 from tkinter import *
-from LoginLogic import data_connect
+
+import logindatabase
+
 
 #BUTTON ACTION
 def click():
         username = userentry.get()
         password = passentry.get()
-        data_connect(username,password)        
+        logindatabase.login(username, password)  
         userentry.delete(0, END)
         passentry.delete(0, END)
 
 
-#WINDOW
+#WINDOW PANE
 window = Tk()
 window.title("Login")
 
-#LABELS
+#USERNAME LABEL
 Label (window, text="Username", fg = "black").grid(row = 0, column = 0, sticky = W)
+#PASSWORD LABEL
 Label (window, text="Password", fg = "black").grid(row = 1, column = 0, sticky = W)
 
-#USERNAME AND PASSWORD ENTRY
+#USERNAME ENTRY
 userentry = Entry(window, width = 20, bg = "white")
 userentry.grid(row = 0, column = 1, sticky = W)
+#PASSWORD ENTRY
 passentry = Entry(window, width = 20, bg = "white")
 passentry.grid(row = 1, column = 1, sticky = W)
 passentry.config(show = "*")
---
+++ LoginApp/logindatabase.py
@@ -0,0 +1,62 @@
+
+def login(username, password):
+
+    #MYSQL connector and connection.
+    import mysql.connector
+    mydb = mysql.connector.connect(host = "localhost", user = "root", passwd = "Durks.321", database = "login")
+
+    #Create cursor.
+    cursor = mydb.cursor()
+ 
+    #FIND_USER and CREATE_USER MYSQL lines.
+    find_user = ("SELECT * FROM user_login WHERE user_name = %s AND user_pass = %s")
+    create_user = ("INSERT INTO user_login (user_name, user_pass) VALUES (%s, %s)")
+    new_user = ("SELECT * FROM user_login WHERE user_name = %s")
+
+    #Cursor execute find user with username and password from input. Result is fetched.
+    cursor.execute(find_user, [(username),(password)])
+    result = cursor.fetchall()
+    if result:
+        for i in result:
+            print("Welcome Back " + i[1] + ".")
+            break
+    else:
+        #If username and password does not match, ask user to create a new account.
+        print("Username and password combination not recognized.")
+        loop = 1
+        while loop == 1:
+            answer = input("Would you like to create an account? y/n: \n")
+            if answer == "y":
+                #Ask for new username and password.
+                username = input("Enter new Username: ")
+                password = input("Enter new Password: ")
+                cursor.execute(new_user, [(username)])
+                result = cursor.fetchall()
+                if result:
+                    for k in result:
+                        answer = input("Account already exists. Would you like to try again? y/n: \n")
+                        if answer == "n":
+                            loop = 0
+                            print("Goodbye!")
+                        break
+                else:
+                    cursor.execute(create_user, [(username),(password)])
+                    mydb.commit()
+                    cursor.execute(find_user, [(username),(password)])
+                    result = cursor.fetchall()
+                    if result:
+                        for j in result:
+                            print("Welcome " + j[1])
+                            break
+            else:
+                loop = 0
+                print("Goodbye!")
+
+
+
+    
+    
+    
+
+
+
