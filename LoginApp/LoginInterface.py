3eb9afe47917d02a5fb7295cb4c680ff17c0fdf6
Ale Alex <alejandroravelo42@gmail.com>
2019-09-26 19:13:23 -0400

    mna

--- LoginApp/LoginInterface.py
+++ LoginApp/LoginInterface.py
@@ -3,14 +3,19 @@ import tkinter as tk
 HEIGHT = 500
 WIDTH = 600
 
+
+
+
+def btnpress(urs,pws):
+    print("Button Clicked!")
+    print(urs)
+    print(pws)
+
+
 root = tk.Tk()
 
 canvas = tk.Canvas(root, height=HEIGHT, width=WIDTH)
 canvas.pack()
-
-button = tk.Button(root, text="Log In", )
-button.pack()
-
 
 label = tk.LabelFrame(root, text="Username")
 label.place(relx=0.05, rely=0.05, relwidth=0.15, relheight=0.05)
@@ -20,13 +25,19 @@ label = tk.LabelFrame(root, text="Passworld")
 label.place(relx=0.05, rely=0.10, relwidth=0.15, relheight=0.05)
 
 def Uname():
- entry = tk.Entry(root)
- entry.place(relx=0.20, rely=0.05, relwidth=0.15, relheight=0.05)
-
+ urs = tk.Entry(root)
+ urs.place(relx=0.20, rely=0.05, relwidth=0.15, relheight=0.05)
+ urs.get()
 def Upass():
- entry = tk.Entry(root)
- entry.place(relx=0.20, rely=0.10, relwidth=0.15, relheight=0.05)
+ pws = tk.Entry(root)
+ pws.place(relx=0.20, rely=0.10, relwidth=0.15, relheight=0.05)
+ pws.get()
+
+button = tk.Button(root, text="Log In", command=btnpress)
+button.pack()
 
 Uname()
 Upass()
+
+
 root.mainloop()
