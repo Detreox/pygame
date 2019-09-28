68014156432d4b8dffd4388f5ae75b5d514d2998
Ale Alex <alejandroravelo42@gmail.com>
2019-09-28 01:55:56 -0400

    ad

--- LoginApp/LoginInterface.py
+++ LoginApp/LoginInterface.py
@@ -1,4 +1,3 @@
-
 from tkinter import *
 
 import logindatabase
@@ -10,12 +9,34 @@ window.geometry("500x400")
 window.resizable(0, 0)
 
 #BUTTON ACTION
-def click():
+def log():
         username = userentry.get()
         password = passentry.get()
-        logindatabase.login(username, password)  
+        logindatabase.login(username, password)
         userentry.delete(0, END)
         passentry.delete(0, END)
+
+def reg():
+        register = Tk()
+        register.title("Register")
+        register.geometry("500x400")
+        register.resizable(0, 0)
+
+        #USERNAME LABEL
+        Label (register, text="Username", fg = "black").grid(row = 0, column = 2)
+        #PASSWORD LABEL
+        Label (register, text="Password", fg = "black").grid(row = 1, column = 2)
+
+        userentry = Entry(register, width = 20, bg = "white")
+        userentry.grid(row = 0, column = 3)
+
+        passentry = Entry(register, width = 20, bg = "white")
+        passentry.grid(row = 1, column = 3)
+        passentry.config(show = "*")
+
+        Button(window, text = "REGISTER", font=40, width = 6, command = log).grid(row = 5, column = 3)
+
+
 
 #USERNAME LABEL
 Label (window, text="Username", fg = "black").grid(row = 0, column = 2)
@@ -31,9 +52,9 @@ passentry.grid(row = 1, column = 3)
 passentry.config(show = "*")
 
 #LOGIN BUTTON
-Button(window, text = "LOGIN", font=40, width = 6, command = click).grid(row = 5, column = 3)
+Button(window, text = "LOGIN", font=40, width = 6, command = log).grid(row = 5, column = 3)
 
 #REGISTER BUTTON
-Button(window, text = "LOGIN", font=40, width = 6, command = click).grid(row = 5, column = 3)
+Button(window, text = "REGISTER", font=40, width = 6, command = reg).grid(row = 5, column = 5)
 
-window.mainloop() 
+window.mainloop()
